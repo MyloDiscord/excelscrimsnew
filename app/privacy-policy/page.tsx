8b99408bbea597c185a27ae1f1f4e059bd989462
MyloDiscord <mylobusiness1@proton.me>
2025-06-26 01:58:09 +1000

    xc

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link"; // <-- import Link
+import Link from "next/link";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function HomePage() {
