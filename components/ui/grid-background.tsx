e70fb581610fc5cc896f95443589d3ce4ece3ca9
KotaAU <ec987527@gmail.com>
2025-07-09 22:58:30 +1000

    fghfgfg

--- components/ui/grid-background.tsx
+++ components/ui/grid-background.tsx
@@ -11,8 +11,13 @@ export function GridBackgroundDemo() {
           "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
           "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
         )}
+        style={{
+          maskImage:
+            "radial-gradient(ellipse at center, white 60%, transparent 100%)",
+          WebkitMaskImage:
+            "radial-gradient(ellipse at center, white 60%, transparent 100%)",
+        }}
       />
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-transparent dark:bg-black"></div>
       <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
         Backgrounds
       </p>
